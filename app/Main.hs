db7da4278702456ed981ac66db78bf63d180b4ee
Shaolong Li <shaolongli26@gmail.com>
2023-12-14 16:29:07 -0800

    add reset

--- app/Main.hs
+++ app/Main.hs
@@ -11,3 +11,7 @@ main = runUI
 
 
 
+
+
+
+
--- src/UI.hs
+++ src/UI.hs
@@ -22,8 +22,6 @@ import qualified Graphics.Vty as V
 captionAttr :: AttrName
 captionAttr = attrName "caption"
 
-
-
 -- Define the cell and grid types
 data Cell = Empty | X | O deriving (Show, Eq)
 data CellCursor = CC {cell :: Cell, strong :: Bool} deriving (Show, Eq)
@@ -74,7 +72,6 @@ drawGrid grid =
         . vBox $ map drawRow grid
 
 
-
 -- try to fit the board data type into the UI
 uiRepresentation :: Board -> [[Cell]]
 uiRepresentation board =
@@ -95,18 +92,12 @@ currentPlayerString game = "Current Player: " ++ name player
   where
     player = players game !! nextPlayer game
 
--- boomsLeftString :: String -> Int -> String
--- boomsLeftString playerName count = "Boom left for " ++ playerName ++ ": " ++ show count
 
-boomsLeftStringPlayer1 :: Game -> String
-boomsLeftStringPlayer1 game = "Boom left for " ++ (name player) ++ ": " ++ show (boomsLeft player)
+boomsLeftStringPlayer :: Game -> Int -> String
+boomsLeftStringPlayer game n = "Boom left for " ++ name player ++ ": " ++ show (boomsLeft player)
   where
-    player = players game !! 0
+    player = players game !! n
 
-boomsLeftStringPlayer2 :: Game -> String
-boomsLeftStringPlayer2 game = "Boom left for " ++ (name player) ++ ": " ++ show (boomsLeft player)
-  where
-    player = players game !! 1
 
 displayWinner :: Game -> String
 displayWinner game = if isWin (board game)
@@ -115,8 +106,13 @@ displayWinner game = if isWin (board game)
   where
     player = players game !! nextPlayer game
 
-mapWithIndex :: ((Int, Int) -> a -> b) -> [[a]] -> [[b]]
-mapWithIndex f = zipWith (\y row -> zipWith (\x a -> f (x, y) a) [0..] row) [0..]
+
+displayScorePlayer :: Game -> Int -> String
+displayScorePlayer game n = name player ++ " score: " ++ show (score player)
+  where
+    player = players game !! n
+
+
 
 drawUI :: Game -> Widget ()
 drawUI game =
@@ -132,64 +128,23 @@ drawUI game =
                 drawGrid cellCursors,
             vBox [  -- Change here to vBox for vertical alignment
                 str (currentPlayerString game),
-                str (boomsLeftStringPlayer1 game),
-                str (boomsLeftStringPlayer2 game),
-                str (displayWinner game)
+                str (boomsLeftStringPlayer game 0),
+                str (boomsLeftStringPlayer game 1),
+                str (displayWinner game),
+                str (displayScorePlayer game 0),
+                str (displayScorePlayer game 1)
             ]
         ]
 
 
-testBoard :: [[Int]]
-testBoard = [[-1, -1,-1,-1,-1,-1],
-                  [-1,-1, -1,-1,-1,-1],
-                  [-1,-1,-1, -1,-1,-1],
-                  [-1,-1,-1,-1, -1,-1],
-                  [-1,-1,-1,-1,-1, -1],
-                  [-1,-1,-1,-1,-1,-1]]
-
-
-cursorPos :: Game -> Location
-cursorPos game = let (x, y) = cursor game in Location (leftOffset + screenX x, topOffset + screenY y)
-
 runUI :: IO Game
 runUI = defaultMain app (init1 10 ["Player 1", "Player 2"])
--- These offsets should represent the starting point of your grid on the screen.
-topOffset :: Int
-topOffset = 18 -- Number of lines before the board starts, adjust accordingly
-
-leftOffset :: Int
-leftOffset = 84 -- Number of characters from the left of the screen, adjust accordingly
-
--- Assume each cell is 5 characters wide including the borders and padding
-cellWidth :: Int
-cellWidth = 5 -- Change this according to your actual cell width
-
--- Assume each cell is 3 characters tall including the borders and padding
-cellHeight :: Int
-cellHeight = 3 -- Change this according to your actual cell height
-
--- Converts game coordinates to screen coordinates
-screenX :: Int -> Int
-screenX x = x * cellWidth
-
-screenY :: Int -> Int
-screenY y = y * cellHeight
-
-
-displayCursor :: Game -> [CursorLocation n] -> Maybe (CursorLocation n)
-displayCursor game cursors = Just $ CursorLocation
-    { cursorLocation = cursorPos game
-    , cursorLocationName = Nothing
-    , cursorLocationVisible = False
-    }
-
-
 
 
 app :: App Game e ()
 app = App
   { appDraw         = \game -> [drawUI game]   -- Use the modified drawUI function
-  , appChooseCursor =  displayCursor
+  , appChooseCursor =  neverShowCursor
   , appHandleEvent  = handleEvent
   , appStartEvent   = pure
   , appAttrMap      = const theMap
@@ -197,19 +152,23 @@ app = App
 
 
 
-
 handleEvent :: Game -> BrickEvent () e -> EventM () (Next Game)
 handleEvent game (VtyEvent (V.EvKey (V.KChar 'z') [V.MCtrl])) = halt game
-handleEvent game (VtyEvent (V.EvKey key [])) = case key of
-  V.KUp  -> continue (moveCursor game T.Up)
-  V.KDown -> continue (moveCursor game T.Down)
-  V.KLeft -> continue (moveCursor game T.Left)
-  V.KRight -> continue (moveCursor game T.Right)
-  V.KChar 'u' -> continue (undo game)
-  V.KChar 'b' -> continue (fst $ boom game)
-  V.KEnter -> continue (fst $ dodo game)
-  _ -> continue game
+handleEvent game (VtyEvent (V.EvKey key [])) = if isWin (board game)
+  then case key of
+    V.KChar 'r' -> continue (reset game)
+    _ -> continue game
+  else case key of
+    V.KUp  -> continue (moveCursor game T.Up)
+    V.KDown -> continue (moveCursor game T.Down)
+    V.KLeft -> continue (moveCursor game T.Left)
+    V.KRight -> continue (moveCursor game T.Right)
+    V.KChar 'u' -> continue (undo game)
+    V.KChar 'b' -> continue (fst $ boom game)
+    V.KEnter -> continue (fst $ dodo game)
+    _ -> continue game
 handleEvent game _ = continue game
+
 
 
 
